b4b2d43509094ce9ea99390e784b34ac88b18fcb
Pedro Daniel Gutierrez Garcia <Jose-Miguel.Mercado@gft.com>
2024-08-10 23:17:10 -0600

    1

--- app/src/main/java/com/example/data/repository/ProductRepository.kt
+++ app/src/main/java/com/example/data/repository/ProductRepository.kt
@@ -4,6 +4,7 @@ import com.example.data.api.RetrofitClient
 import com.example.data.model.ProductResponse
 
 class ProductRepository {
+
     private val apiService = RetrofitClient.apiService
 
     suspend fun fetchProducts(searchString: String, pageNumber: Int): ProductResponse {
